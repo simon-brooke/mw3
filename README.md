4dd486824d18ce60d0dd2032b788350c1439966d
Simon Brooke <simon@journeyman.cc>
2016-02-15 16:34:04 +0000

    Now successfully rendering the replacement navigation, but not yet made it work.

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 MW3 is intended to be a re-implementation of MicroWorld in ClojureScript: the third reimplementation, although not nearly as dramatic as the shift from C to Clojure!
 
 It is a very long way from finished and does not even nearly work yet. Come back in a month or two.
+
+## What I'm trying to achieve
+
+What I'd really like to achieve is
+
+1. A single page application, that
+2. If JavaScript is available in the browser, runs mainly or wholly client side, but
+3. If JavaScript isn't available, runs wholly server side
+4. With as little as possible difference between the two modes.
+
+This may not be possible, but does account for the fact that navigation is done statically in the HTML, and then re-done in the ClojureScript.
--- src/cljs/mw3/core.cljs
+++ src/cljs/mw3/core.cljs
@@ -1,4 +1,5 @@
 (ns ^:figwheel-always mw3.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! chan <!]]
@@ -8,22 +9,25 @@
 
 ;; The state of the application.
 ;; NOTE that app-state can contain vectors and maps only, not lists.
-(defonce app-state (atom {:available-tabs {:#home-tab :#home-content
-                                           :#params-tab :#params-content
-                                           :#rules-tab :#rules-content
-                                           :#world-tab :#world-content
-                                           :#docs-tab :#docs-content}
-                          :selected-tab :#home-tab
-                          :text "Welcome to MicroWorld!"
-                          :rules []
+(defonce app-state (atom {;; Available tabs/pages within the application
+                          :available-tabs {:#home-tab {:content :#home-content :text "Home"}
+                                           :#params-tab {:content :#params-content :text "Parameters"}
+                                           :#rules-tab {:content :#rules-content :text "Rules"}
+                                           :#world-tab {:content :#world-content :text "World"}
+                                           :#docs-tab {:content :#docs-content :text "Documentation"}}
+                          ;; Parameters, set up on the parameters page.
                           :params {:available-rulesets []
                                    :available-heightmaps []
                                    :ruleset ""
                                    :heightmap ""}
+                          ;; Currently active rules.
+                          :rules []
+                          ;; Currently selected tab.
+                          :selected-tab :#home-tab
+                          ;; Junk so I can see stuff during development.
+                          :text "Welcome to MicroWorld!"
+                          ;; The current state of the world.
                           :world []}))
-
-(deref app-state)
-(:available-tabs (deref app-state))
 
 (defn root-component [data owner]
   (reify
@@ -35,31 +39,29 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:selected-tab :#home-tab
-       :selected-content :#home-content})
-    om/IRender
-    (render [this]
-            (dmu/ul nil
-                    )))
+                {:selected-tab :#home-tab
+                 :selected-content :#home-content})
+;;     om/IWillMount
+;;     (will-mount [_]
+;;                 (let [selection (om/get-state owner :selected)]
+;;                   (om/transact! data :selected-tab selection)))
+    om/IRenderState
+    (render-state [this state]
+                  (apply dom/ul #js {:id "tab-bar"}
+                         (om/build-all
+                          (fn [key]
+                            (reify
+                              om/IRenderState
+                              (render-state [this {:keys [selected]}]
+                                            (dom/li
+                                             #js {:onClick (fn [e] (put! selected @key))
+                                                  :className (if (= key (:selected-tab data)) "selected" "tab")}
+                                             (:text (key (:available-tabs data)))))))
+                          (keys (:available-tabs data)){:init-state state})))))
 
 
 (def pages [:#home-content :#params-content :#rules-content :#world-content :#docs-content])
 
-(defn onclick-handler
-  "Handles a click on the specified `tab` and reveals the specified `content`."
-  [tab content]
-  (doseq [page pages]
-          (dommy/hide!(sel page)))
-  (dommy/show! content))
-
-;; NASTY! Javascript interprets hyphens as subtraction operators, so function names
-;; exposed to raw Javascript (e.g. onclick event handlers) must not contain hyphens.
-
-(defn ^:export home []
-  (onclick-handler :#home-tab :#home-content))
-
-(defn ^:export params []
-  (onclick-handler :#params-tab :#params-content))
 
 
 (om/root
@@ -67,4 +69,7 @@
  app-state
  {:target (. js/document (getElementById "world-content"))})
 
-
+(om/root
+ tab-bar
+ app-state
+ {:target (. js/document (getElementById "tab-bar-target"))})
